0f442f92b3829a4a0307c6c26093f3aa38cd933d
LuMi2602 <wedimulie@gmail.com>
2023-05-17 00:07:43 +0400

    Frontend

---
+++ x_classes.js
@@ -0,0 +1,93 @@
+let x_classes  = [   
+    {
+    "id": 1,
+    "Image": "Images_For_Subjects/french.jpg",
+    "Subject":"French", 
+    "Location": "Room 105",
+    "Price": 800 + " AED",
+    "Space": 5
+
+       }, 
+    
+    {
+        "id": 2,
+        "Image": "Images_For_Subjects/coding.jpg",
+        "Subject":"Coding", 
+        "Location": "Room 107",
+        "Price": 700 + " AED",
+        "Space": 5    
+    }, 
+
+    {
+      "id": 3,
+      "Image": "Images_For_Subjects/art.jpg",
+      "Subject":"Art", 
+      "Location": "Room 110",
+      "Price": 750 + " AED",
+      "Space": 5     
+  }, 
+
+   {
+   "id": 4,
+   "Image": "Images_For_Subjects/music.jpg",
+   "Subject":"Music", 
+   "Location": "Music Room",
+   "Price": 600 + " AED",
+   "Space": 5     
+      }, 
+
+      {
+         "id": 5,
+         "Image": "Images_For_Subjects/creative_writing.jpg",
+         "Subject":"Creative Writing", 
+         "Location": "Room 021",
+         "Price": 600 + " AED",
+         "Space": 5    
+            }, 
+
+        
+         {
+            "id": 6,
+            "Image": "Images_For_Subjects/football.jpg",
+            "Subject":"Football ", 
+            "Location": "Football Field",
+            "Price": 600 + " AED",
+            "Space": 5     
+               }, 
+         
+     
+               {
+                  "id": 7,
+                  "Image": "Images_For_Subjects/dancing.jpg",
+                  "Subject":"Dancing", 
+                  "Location": "Main Hall",
+                  "Price": 800 + " AED",
+                  "Space": 5     
+             },  
+
+             {
+               "id": 8,
+               "Image": "Images_For_Subjects/basketball.jpg",
+               "Subject":"Basketball", 
+               "Location": "Basketball Court",
+               "Price": 600 + " AED",
+               "Space": 5     
+          },  
+             
+             {
+               "id": 9,
+               "Image": "Images_For_Subjects/acting.jpg",
+               "Subject":"Acting", 
+               "Location": "School Theatre",
+               "Price": 600 + " AED",
+               "Space": 5     
+          }, 
+          {
+            "id": 10,
+            "Image": "Images_For_Subjects/singing.jpg",
+            "Subject":"Singing", 
+            "Location": "Room 220",
+            "Price": 800 + " AED",
+            "Space": 5     
+       }, 
+        ]
